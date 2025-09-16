e2b93fd18af2166b8d3d8d2ed424a021fcccefc3
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-09-16 20:31:13 -0300

    LCS: Conclui o algoritimo de LCS.
    - Realiza o backtracking, percorrendo a matriz do final até o começo, montando a LCS.

--- lcs/src/main.rs
+++ lcs/src/main.rs
@@ -4,8 +4,7 @@ fn main() {
     let entries_b = vec!["G", "X", "T", "X", "A", "Y", "B"]; 
 
     let mut dp: Vec<Vec<i32>> = vec![vec![0; entries_b.len() + 1]; entries_a.len() + 1];
-    let mut lcs_len = 0;
-    
+    let lcs_len = dp[entries_a.len()][entries_b.len()];
 
     for i in 1..=entries_a.len() {
         for j in 1..=entries_b.len()  {
@@ -18,7 +17,26 @@ fn main() {
         }
     }
 
-    lcs_len = dp[entries_a.len()][entries_b.len()];
-
     println!("o tamanho da LCS é: {}", lcs_len);
+
+    let mut i = entries_a.len();
+    let mut j = entries_b.len();
+    let mut lcs: Vec<&str> = Vec::new();
+
+    while i > 0 && j > 0 {
+        if entries_a[i-1] == entries_b[j-1] {
+            lcs.push(entries_a[i-1]);
+            j -= 1;
+            i -= 1;
+        }
+        else if dp[i-1][j] > dp[i][j-1] {
+            i -= 1;
+        }
+        else {
+            j -= 1;
+        }
+    }
+
+    lcs.reverse();
+    println!("LCS completa é: {:?}", lcs);
 }
