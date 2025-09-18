ead9418792ff50ac1b5affd45c641189e51e6fa3
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-09-17 21:45:51 -0300

    SES: Corrige indices.

--- ses/src/main.rs
+++ ses/src/main.rs
@@ -5,10 +5,10 @@ fn main() {
     let mut dp: Vec<Vec<i32>> = vec![vec![0; entries_b.len() + 1]; entries_a.len() + 1];
 
     for i in 1..=entries_a.len()  {
-        dp[0][i] = i as i32;
+        dp[i][0] = i as i32;
     }
 
     for j in 1..=entries_b.len()  {
-        dp[j][0] = j as i32;
+        dp[0][j] = j as i32;
     }
 }
