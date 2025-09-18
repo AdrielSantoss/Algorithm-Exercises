c77b51f1c50df8d3689cd9e0246a041b2dfbbd77
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-09-17 21:51:46 -0300

    SES: Calcula e exibe o cumprimento/distancia da SES.

--- ses/src/main.rs
+++ ses/src/main.rs
@@ -11,4 +11,17 @@ fn main() {
     for j in 1..=entries_b.len()  {
         dp[0][j] = j as i32;
     }
+
+    for i in 1..=entries_a.len() {
+        for j in 1..=entries_b.len() {
+            if entries_a[i-1] == entries_b[j-1] {
+                dp[i][j] = dp[i-1][j-1]; // match: recebe diagonal
+            }
+            else {
+                dp[i][j] = 1 + dp[i-1][j].min(dp[i-1][j-1]).min(dp[i][j-1]); // missmatch: mais rapido entre deletar, inserir ou substituir
+            }
+        }
+    }
+
+    println!("Distância de edição (SES): {}", dp[entries_a.len()][entries_b.len()]);
 }
