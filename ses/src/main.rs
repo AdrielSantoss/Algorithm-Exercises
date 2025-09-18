62280ec17d0efdce32b88347a881d6a571c235a0
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-09-17 21:42:44 -0300

    SES: Adiciona montagem inicial da dp.

--- ses/src/main.rs
+++ ses/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let entries_a = vec!["A", "G", "G", "T", "A", "B"];
+    let entries_b = vec!["G", "X", "T", "X", "A", "Y", "B"];
+
+    let mut dp: Vec<Vec<i32>> = vec![vec![0; entries_b.len() + 1]; entries_a.len() + 1];
+
+    for i in 1..=entries_a.len()  {
+        dp[0][i] = i as i32;
+    }
+
+    for j in 1..=entries_b.len()  {
+        dp[j][0] = j as i32;
+    }
 }
